cb4484745a5323ba5fe4009b7c9946ca391d6cb5
Ashley Young <burning.rose85@gmail.com>
2017-03-10 23:24:21 -0500

    PHP output and query testing

--- tests.d/basic.php
+++ tests.d/basic.php
@@ -12,6 +12,9 @@ if (!mysql_select_db('kn_fsm', $link)) {
 }
 
 $sql    = 'select * from ice_time, coaches, locations, ice_type where ice_time.coach_id = ice_time.coach_id and coaches.id = ice_time.coach_id and locations.id = ice_time.rink_id and ice_type.id = ice_time.skate_type';
+#$result = mysql_query($sql, $link);
+
+#$sql = 'select * from ice_time';
 $result = mysql_query($sql, $link);
 
 if (!$result) {
@@ -20,14 +23,23 @@ if (!$result) {
     exit;
 }
 
-echo $result;
+$total_ice = 0;
+$total_coaching_cost = 0;
 
-#while ($row = mysql_fetch_assoc($result)) {
-#    echo $row['foo'];
-#    echo $sql;
+while ($row = mysql_fetch_assoc($result)) {
+    $total_ice += $row['ice_time'];
+    echo "<p>".$row['date']." ".$row['ice_time']." ".$row['ice_cost']." ".$row['type']." ".$row['coach_time']." ".$row['coach_fname']." ".$row['coach_lname']." ".$row['coach_rate']." ".$row['location_id']." ".$row['location_city']." ".$row['location_state']."</p>";
+    echo "\n";
+    $total_coaching_cost = $row['coach_rate'];
+
 }
 
-mysql_free_result($result);
+$ice_hours = $total_ice / 60;
+$coachingcosts = (($total_coaching_cost * 2) *  $ice_hours);
+
+echo "<p>".$total_ice / 60 ." Hours </p>";
+echo "<p>".$coachingcosts." Dollars </p>";
+#mysql_free_result($result);
 
 ?>
 

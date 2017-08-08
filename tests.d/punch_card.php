72ac039c06d86012788fdec65141bd13bd4b6e40
Ashley Young <burning.rose85@gmail.com>
2017-08-08 18:14:54 -0400

    updating test punch logic to add punch value, show table

--- tests.d/punch_card.php
+++ tests.d/punch_card.php
@@ -10,13 +10,18 @@ require '../include.d/db_connect.php';
     		echo 'MySQL Error: ' . mysql_error();
     		exit;
 	}
-        
+	echo "<table>";       
+	echo "<tr><td> Date </td><td> Time </td><td> Cost </td><td> Punches </td></tr>";
 	while ($row = mysql_fetch_assoc($result)) {
         $skate_total += $row['ice_time'];
-        }
-        
+	$punch_down = $row['ice_time'] / 30;
+        echo "<tr><td>" . $row['date'] . "</td><td>" . $row['ice_time'] . "</td><td>" . $row['ice_cost'] . "</td><td>" . $punch_down  . "</td></tr>";
+ 	$punches_total += $punch_down;
+
+       }
+        echo "</table>";
 	echo '<p>' . $skate_total . ' total minutes</p>';
-       return array($skate_total);
+       return array($skate_total, $punches_total);
 }
 
 function punch_card(){
@@ -42,10 +47,12 @@ require '../include.d/db_connect.php';
 $skate = skate_total();
 $punch = punch_card();
 $skate_h = $skate[0] / 30;
+$punched = $skate[1];
 $diff = $punch[0] - $skate[0];
 $conv = $diff / 30;
 $card = $punch[0] / 30;
 
+echo '<p>' . $punched . ' used punches';
 echo '<p>' . $conv . ' punches remaining';
 echo '<p>' . $card . ' total on card';
 ?>

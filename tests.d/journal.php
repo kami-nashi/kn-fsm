0a62831cd65e60263b2ef82f927507edce7551e7
Ashley Young <burning.rose85@gmail.com>
2017-10-07 18:24:08 -0400

    dumping list of video urls for journal

---
+++ tests.d/journal.php
@@ -0,0 +1,30 @@
+<?php
+
+function journal_videos() {
+
+        require '../include.d/db_connect.php';
+        $get_sql = 'SELECT ice_time.*, j_videos.* FROM ice_time, j_videos WHERE ice_time.date = j_videos.date';
+        $coach_result = mysql_query($get_sql, $link);
+                if (!$coach_result) {
+                echo "DB Error, could not query the database\n";
+                echo 'MySQL Error: ' . mysql_error();
+                exit;
+         }
+
+	echo "<table>";
+	echo "<tr><td> Date </td><td> Sharing Platform </td><td> URL <tr><td>";
+
+        while ($row = mysql_fetch_assoc($coach_result)) {
+                $jv_date += $row['date'];
+                $jv_url = $row['url'];
+
+    		echo "<tr><td>" . $row['date'] . "</td><td>" . $row['sharing_platform']  . "</td><td>" . "<a href='" . $row['url'] . "'>" . $row['url'] . "</a>"  . "<tr><td>";
+    		echo "\n";
+        }
+	echo "</table>";
+        return array($jv_date, $jv_url);
+}
+
+journal_videos();
+
+?>

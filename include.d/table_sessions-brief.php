c0863acdcb85297f7d02759fab86c522c96831c7
Ashley Young <burning.rose85@gmail.com>
2017-10-08 22:02:46 -0400

    adding same journal features as before but to brief session files

--- include.d/table_sessions-brief.php
+++ include.d/table_sessions-brief.php
@@ -3,7 +3,7 @@
 require 'db_connect.php';
 
 echo '<table class="table table-striped"> <thead>
-<tr><td>Date</td><td>Skate Time</td><td>Ice Cost</td><td>Skate Type</td><td>Coach Time</td><td>Coach Name</td><td>Coach Rate</td><td>Rink Name</td><td>City</td><td>State</td></tr>
+<tr><td>Date</td><td>Skate Time</td><td>Ice Cost</td><td>Skate Type</td><td>Coach Time</td><td>Coach Name</td><td>Coach Rate</td><td>Journal</td><td>Rink Name</td><td>City</td><td>State</td></tr>
 
 </thead>
 <tbody>';
@@ -19,9 +19,14 @@ if (!$result) {
 while ($row = mysql_fetch_assoc($result)) {
     $total_ice += $row['ice_time'];
     $ice_cost += $row['ice_cost'];
+    $jv = '--';
+    $jv_url = urlencode($row['date']) . "\n";
 
-    echo "<tr><td>" . $row['date'] . "</td><td>" . $row['ice_time'] . "</td><td>" . $row['ice_cost'] . "</td><td>" .$row['type'] . "</td><td>" . $row['coach_time'] . "</td><td>" . $row['coach_fname'] . " " . $row['coach_lname'] . "</td><td>" . $row['coach_rate'] . "</td><td>" . $row['location_id'] . "</td><td>" . $row['location_city'] ."</td><td>" .  $row['location_state'] . "</td></tr>";
-    
+       if ($row['has_video'] == 1)
+	$jv = '<a href="journal.php?date=' . $jv_url . '" ><img src="images/booklet.png" style="width:32px;height:32px;"></a>';
+
+    echo "<tr><td>" . $row['date'] . "</td><td>" . $row['ice_time'] . "</td><td>" . $row['ice_cost'] . "</td><td>" .$row['type'] . "</td><td>" . $row['coach_time'] . "</td><td>" . $row['coach_fname'] . " " . $row['coach_lname'] . "</td><td>" . $row['coach_rate'] . "</td><td>" . $jv . "</td><td>" . $row['location_id'] . "</td><td>" . $row['location_city'] ."</td><td>" .  $row['location_state'] . "</td></tr>";
+ 
     $sum_coach_time += $row['coach_time'];
     $converted_coach_rate = $row['coach_rate'] / 30;
     $final_coach_rate += $row['coach_time'] * $converted_coach_rate;
